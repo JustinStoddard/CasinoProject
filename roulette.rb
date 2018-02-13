4944924e4e897be5036607b06e3a8b710c1dd03a
Bradley Findeis <bradleyfindeis@Bradleys-MacBook.local>
2018-02-12 17:33:02 -0700

    Roulette end of calss additions

--- roulette.rb
+++ roulette.rb
@@ -1,18 +1,10 @@
-# numbers are 0 - 36 but 0 does not count
-# numbers go "0-32-15-19-4-21-2-25-17-34-6-27-13-36-11-30-8-23-10-5-24-16-33-1-20-14-31-9-22-18-29-7-28-12-35-3-26"
-# Red numbers are "1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23,25, 27, 30, 32, 34, 36"
-# Black numbers are "2, 4, 6, 8, 10, 11, 13, 15, 17, 20, 22, 24, 26, 28, 29, 31, 33, 35"
-# bets are: 0 - are 35:1 odds, straight up - any single number 35:1, odd or even - 2:1, red or black - 2:1
-# Create win or lose functions
-# Create link back to the main menu
-# require_relative #'player'
-# require_relative #'casino'
+
 require_relative 'spoke'
 require 'pry'
+
 class Roulette
     attr_accessor :numbers, :wheel, :customer
         
-    
     def initialize(customer)
         @numbers = []
         @wheel = []
@@ -34,10 +26,12 @@ class Roulette
         end 
         shuffle
     end
+
     def shuffle
        @lucky = @wheel.sample
         bets_menu
     end
+
     def bets_menu
         puts "-----------------------------"
         puts "--------- Roulette ----------"
@@ -54,6 +48,7 @@ class Roulette
         bets_menu_choice = gets.to_i
         the_bets_menu(bets_menu_choice)
     end
+
     def the_bets_menu(bets_menu_choice)
             if bets_menu_choice == 1
                 bet_on_zero_game
@@ -68,12 +63,13 @@ class Roulette
             elsif bets_menu_choice == 6
                 bet_on_red
             elsif bets_menu_choice == 7
-                casino_menu
+                
             else
                 "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
             end
         
     end
+
     def bet_on_zero_game
         puts "You have #{@customer.wallet} in your wallet"
         puts " "
@@ -85,22 +81,22 @@ class Roulette
             print "            \r"
             puts "spinning"
             puts "Color:#{@lucky.color} Number:#{@lucky.number}"
-            if @lucky.number == 0
-               winnings = bet * 35
-                @wallet += winnings
-            else
-                @wallet -= bet
-            end
-            puts "You have $#{@wallet} left. Do you want to spin again on the Bet On 0 game? Please press 'Y' or 'N'"
-             leave = gets.strip.upcase
-            if leave == 'Y'
-                bet_on_zero_game
-            elsif leave == 'N'
-                bets_menu
-            else
-                puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
-                bets_menu
-            end
+                if @lucky.number == 0
+                winnings = bet * 35
+                    @customer.wallet += winnings
+                else
+                    @customer.wallet -= bet
+                end
+                puts "You have $#{@customer.wallet} left. Do you want to spin again on the Bet On 0 game? Please press 'Y' or 'N'"
+                leave = gets.strip.upcase
+                if leave == 'Y'
+                    bet_on_zero_game
+                elsif leave == 'N'
+                    bets_menu
+                else
+                    puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
+                    bets_menu
+                end
             
         else
             puts "You don't have enough money, you have @{Wallet} in your wallet, please try again."
@@ -108,8 +104,9 @@ class Roulette
             bet_on_0_game
         end
     end
+
     def bet_any_number_game
-        puts "You have #{@wallet} in your wallet"
+        puts "You have #{@customer.wallet} in your wallet"
         puts " "
         puts "What number would you like to bet on?"
         betting_number = gets.to_i
@@ -117,28 +114,28 @@ class Roulette
         puts " "
         puts "How much would you like to bet:"
         bet = gets.to_i
-        if bet <= @wallet
+        if bet <= @customer.wallet
             puts 'press any key to spin the Roulette Wheel!'
             STDIN.gets                                                                                                              
             print "            \r"
             puts "spinning"
             puts "Color:#{@lucky.color} Number:#{@lucky.number}"
-            if @lucky.number == betting_number
-               winnings = bet * 35
-                @wallet += winnings
-            else
-                @wallet -= bet
-            end
-            puts "You have $#{@wallet} left. Do you want to spin again on the Bet On Any Number game? Please press 'Y' or 'N'"
-             leave = gets.strip.upcase
-            if leave == 'Y'
-                bet_any_number_game
-            elsif leave == 'N'
-                bets_menu
-            else
-                puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
-                bets_menu
-            end
+                if @lucky.number == betting_number
+                winnings = bet * 35
+                    @customer.wallet += winnings
+                else
+                    @customer.wallet -= bet
+                end
+                puts "You have $#{@customer.wallet} left. Do you want to spin again on the Bet On Any Number game? Please press 'Y' or 'N'"
+                leave = gets.strip.upcase
+                if leave == 'Y'
+                    bet_any_number_game
+                elsif leave == 'N'
+                    bets_menu
+                else
+                    puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
+                    bets_menu
+                end
             
         else
             puts "You don't have enough money, you have @{Wallet} in your wallet, please try again."
@@ -146,12 +143,14 @@ class Roulette
             bet_any_number_game
         end
     end
+
     def bet_even_game
-        puts "You have #{@wallet} in your wallet"
+
+        puts "You have #{@customer.wallet} in your wallet"
         puts " "
         puts "You are betting on even numbers, how much would you like to bet?"
         bet = gets.to_i
-        if bet <= @wallet
+        if bet <= @customer.wallet
             puts 'press any key to spin the Roulette Wheel!'
             STDIN.gets                                                                                                              
             print "            \r"
@@ -159,22 +158,22 @@ class Roulette
             puts "spinning"
             puts " "
             puts "Color:#{@lucky.color} Number:#{@lucky.number}"
-            if @lucky.number % 2 == 0
-               winnings = bet * 2
-                @wallet += winnings
-            else
-                @wallet -= bet
-            end
-            puts "You have $#{@wallet} left. Do you want to spin again on the Bet On Even Numbers game? Please press 'Y' or 'N'"
-             leave = gets.strip.upcase
-            if leave == 'Y'
-                bet_even_game
-            elsif leave == 'N'
-                bets_menu
-            else
-                puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
-                bets_menu
-            end
+                if @lucky.number % 2 == 0
+                winnings = bet * 2
+                    @custmer.wallet += winnings
+                else
+                    @customer.wallet -= bet
+                end
+                puts "You have $#{@customer.wallet} left. Do you want to spin again on the Bet On Even Numbers game? Please press 'Y' or 'N'"
+                leave = gets.strip.upcase
+                if leave == 'Y'
+                    bet_even_game
+                elsif leave == 'N'
+                    bets_menu
+                else
+                    puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
+                    bets_menu
+                end
             
         else
             puts "You don't have enough money, you have @{Wallet} in your wallet, please try again."
@@ -182,108 +181,119 @@ class Roulette
             bet_even_game
         end
     end
-    # def bet_odd_game
-    #     puts "You have #{@wallet} in your wallet"
-    #     puts " "
-    #    puts "How much would you like to bet:"
-    #    bet = gets.to_i
-    #    if bet <= @wallet
-    #        puts 'press any key to spin the Roulette Wheel!'
-    #        STDIN.getch                                                                                                              
-    #        print "            \r"
-    #        Roulette.new 
-    #        if Roulette.new.number == 0
-    #           winnings = bet * 35
-    #            @wallet + winings
-    #        else
-    #            @wallet - bet
-    #        end
-    
-    #         puts "Do you want to spin again on the Bet On Any Odd Number game? Please press 'Y' or 'N'"
-    #         leave = gets.strip.upcase
-    #        if leave == 'Y'
-    #            bet_odd_game
-    #        elsif leave == 'N'
-    #            casino_menu
-    #        else
-    #            puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
-    #            bets_menu
-    #        end
-    #    else
-    #        puts "You don't have enough money, you have @{Wallet} in your wallet, please try again."
-    #        puts " "
-    #        bet_odd_game
-    #    end
-    # end
-    # def bet_on_black
-    #     puts "You have #{@wallet} in your wallet"
-    #     puts " "
-    #    puts "How much would you like to bet:"
-    #    bet = gets.to_i
-    #    if bet <= @wallet
-    #        puts 'press any key to spin the Roulette Wheel!'
-    #        STDIN.getch                                                                                                              
-    #        print "            \r"
-    #        Roulette.new 
-    #        if Roulette.new.number == 0
-    #           winnings = bet * 35
-    #            @wallet + winings
-    #        else
-    #            @wallet - bet
-    #        end
-       
-    #         puts "Do you want to spin again on the Bet On Any Black game? Please press 'Y' or 'N'"
-    #         leave = gets.strip.upcase
-    #        if leave == 'Y'
-    #            bet_on_black
-    #        elsif leave == 'N'
-    #            casino_menu
-    #        else
-    #            puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
-    #            bets_menu
-    #        end
-    #    else
-    #        puts "You don't have enough money, you have @{Wallet} in your wallet, please try again."
-    #        puts " "
-    #        bet_on_black
-    #    end
-    # end
-    # def bet_on_red
-    #     puts "You have #{@wallet} in your wallet"
-    #     puts " "
-    #    puts "How much would you like to bet:"
-    #    bet = gets.to_i
-    #    if bet <= @wallet
-    #        puts 'press any key to spin the Roulette Wheel!'
-    #        STDIN.getch                                                                                                              
-    #        print "            \r"
-    #        Roulette.new 
-    #        if Roulette.new.number == 0
-    #           winnings = bet * 35
-    #            @wallet + winings
-    #        else
-    #            @wallet - bet
-    #        end
-       
-    #         puts "Do you want to spin again on the Bet On Any Red game? Please press 'Y' or 'N'"
-    #         leave = gets.strip.upcase
-    #        if leave == 'Y'
-    #            bet_on_red
-    #        elsif leave == 'N'
-    #            casino_menu
-    #        else
-    #            puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
-    #            bets_menu
-    #        end
-    #    else
-    #        puts "You don't have enough money, you have @{Wallet} in your wallet, please try again."
-    #        puts " "
-    #        bet_on_red
-    #    end
-    # end
-    # def casino_menu
-    #     #Take me to Casino Menu
-    # end
+
+    def bet_odd_game
+
+        puts "You have #{@customer.wallet} in your wallet"
+        puts " "
+        puts "You are betting on odd numbers, how much would you like to bet?"
+        bet = gets.to_i
+        if bet <= @customer.wallet
+            puts 'press any key to spin the Roulette Wheel!'
+            STDIN.gets                                                                                                              
+            print "            \r"
+            puts " "
+            puts "spinning"
+            puts " "
+            puts "Color:#{@lucky.color} Number:#{@lucky.number}"
+                if @lucky.number % 2 == 0
+                    @customer.wallet -= bet
+                else
+                    winnings = bet * 2
+                    @custmer.wallet += winnings
+                end
+            puts "You have $#{@customer.wallet} left. Do you want to spin again on the Bet On Odd Numbers game? Please press 'Y' or 'N'"
+            leave = gets.strip.upcase
+                if leave == 'Y'
+                    bet_even_game
+                elsif leave == 'N'
+                    bets_menu
+                else
+                    puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
+                    bets_menu
+                end
+        
+        else
+            puts "You don't have enough money, you have @{Wallet} in your wallet, please try again."
+            puts " "
+            bet_odd_game
+        end
+    end
+
+    def bet_on_black
+        puts "You have #{@customer.wallet} in your wallet"
+        puts " "
+        puts "You are betting on all black spots, how much would you like to bet?"
+        bet = gets.to_i
+        if bet <= @customer.wallet
+            puts 'press any key to spin the Roulette Wheel!'
+            STDIN.gets                                                                                                              
+            print "            \r"
+            puts " "
+            puts "spinning"
+            puts " "
+            puts "Color:#{@lucky.color} Number:#{@lucky.number}"
+                if @lucky.color == 'black'
+                    winnings = bet * 2
+                    @custmer.wallet += winnings
+                else
+                    @customer.wallet -= bet
+                end
+
+            puts "You have $#{@customer.wallet} left. Do you want to spin again on the Bet On Black Colors game? Please press 'Y' or 'N'"
+            leave = gets.strip.upcase
+                if leave == 'Y'
+                    bet_on_black
+                elsif leave == 'N'
+                    bets_menu
+                else
+                    puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
+                    bets_menu
+                end
+            
+        else
+            puts "You don't have enough money, you have #{@customer.wallet} in your wallet, please try again."
+            puts " "
+            bet_on_black
+        end
+    end
+
+    def bet_on_red
+        puts " "
+        puts "You are betting on all red spots, how much would you like to bet?"
+        bet = gets.to_i
+        if bet <= @customer.wallet
+            puts 'press any key to spin the Roulette Wheel!'
+            STDIN.gets                                                                                                              
+            print "            \r"
+            puts " "
+            puts "spinning"
+            puts " "
+            puts "Color:#{@lucky.color} Number:#{@lucky.number}"
+                if @lucky.color == 'red'
+                    winnings = bet * 2
+                    @custmer.wallet += winnings
+                else
+                    @customer.wallet -= bet
+                end
+            puts "You have $#{@customer.wallet} left. Do you want to spin again on the Bet On Black Colors game? Please press 'Y' or 'N'"
+                leave = gets.strip.upcase
+                if leave == 'Y'
+                    bet_on_red
+                elsif leave == 'N'
+                    bets_menu
+                else
+                    puts "Invalid Response. Because you can't type, checkout: http://www.broderbund.com/c-33-mavis-beacon.aspx"
+                    bets_menu
+                end
+
+        else
+        puts "You don't have enough money, you have #{@customer.wallet} in your wallet, please try again."
+        puts " "
+        bet_on_red
+        end
+    end
+    # TODO - casino_menu
 end
 
 
